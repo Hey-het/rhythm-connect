d83b228169ece50dd4181abec80e8564abf325ba
Hey-het <hetalbpatelsw@gmail.com>
2025-11-14 12:12:00 +0000

    add taiwind.config

--- client/src/app/content/page.js
+++ client/src/app/content/page.js
@@ -1,38 +1,9 @@
-"use client"
-import { useEffect, useState } from "react"
+import Content from "@/Components/Content";
 
-export default function contantPage() {
-  
-  const [content, setContant] = useState([]);
-  const apiUrl = process.env.NEXT_PUBLIC_API_URL.replace(/\/$/, "");
-
-  console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
-
-  
- useEffect(() => {
-  fetch(`${apiUrl}/content`)
-    .then(res => res.json())
-    .then(data => setContant(data))
-    .catch(err => console.log("Fetch error:", err));
-}, []);
-
-
-
-  return (
-    <>
-      <h1>Welcome to My HomePage</h1>
-      <div>
-       { content.map(item => (
-        <div key ={item.id}
-        >
-             <h2>{item.title}</h2>
-          <p>{item.description}</p>
-            <a href={item.url} target="_blank" rel="noopener noreferrer">Watch</a>
-        </div>
-
-        ))}
-        </div>
-    
-    </>
-  )
+export default function ContentPage(){
+    return (
+        <>
+        <Content />
+        </>
+    )
 }
---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}", // all your pages
+    "./app/**/*.{js,ts,jsx,tsx}",   // if you have an app dir
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
